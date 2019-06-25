8ded254729649d10785aed3706f7a35282acc501
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-25 11:48:18 -0700

    Declared and Initialized a new FirebaseFirestore Instance. Added log Tag

--- app/src/main/java/com/pareenja/carrentalproject/ViewCarsActivity.java
+++ app/src/main/java/com/pareenja/carrentalproject/ViewCarsActivity.java
@@ -1,14 +1,22 @@
 package com.pareenja.carrentalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ViewCarsActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    private static final String TAG = "ViewCarsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_cars);
+
+        db = FirebaseFirestore.getInstance();
     }
 }
