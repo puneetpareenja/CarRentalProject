3f05a8c51db43bf00c8018afb81f18fce7e6442a
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-25 15:53:45 -0700

    Initialized the Fragment widgets and set up an onclicklistner on the TextViews made the textViews clickable and focusable in order to set up the onclick listner

--- app/src/main/java/com/pareenja/carrentalproject/admin/AdminActionFragment.java
+++ app/src/main/java/com/pareenja/carrentalproject/admin/AdminActionFragment.java
@@ -2,12 +2,13 @@ package com.pareenja.carrentalproject.admin;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.pareenja.carrentalproject.R;
 
@@ -15,8 +16,7 @@ import com.pareenja.carrentalproject.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AdminActionFragment extends Fragment {
-
+public class AdminActionFragment extends Fragment implements View.OnClickListener {
 
 
     public AdminActionFragment() {
@@ -28,7 +28,35 @@ public class AdminActionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_action, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_action, container, false);
+        initFragment(view);
+        return view;
     }
 
+    private void initFragment(View view) {
+        TextView mTextViewManageCars = view.findViewById(R.id.text_view_manage_cars);
+        TextView mTextViewManageEmployees = view.findViewById(R.id.text_view_manage_employees);
+        TextView mTextViewManageUsers = view.findViewById(R.id.text_view_manage_users);
+
+        mTextViewManageCars.setOnClickListener(this);
+        mTextViewManageEmployees.setOnClickListener(this);
+        mTextViewManageUsers.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_view_manage_cars:
+                Toast.makeText(getContext(), "Manage Cars", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_view_manage_employees:
+                Toast.makeText(getContext(), "Manage Employees", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_view_manage_users:
+                Toast.makeText(getContext(), "Manage Users", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
