fdf66ca04cc4e5c5c4361215a8ea3fe45e511bf8
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-25 11:02:34 -0700

    Declared and Initialized variables from acitiyity_main.xml in MainActivity.java

--- app/src/main/java/com/pareenja/carrentalproject/MainActivity.java
+++ app/src/main/java/com/pareenja/carrentalproject/MainActivity.java
@@ -3,12 +3,38 @@ package com.pareenja.carrentalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText mUserNameEditText;
+    EditText mPasswordEditText;
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        mUserNameEditText = findViewById(R.id.edit_text_username);
+        mPasswordEditText = findViewById(R.id.edit_text_password);
+        mLoginButton = findViewById(R.id.button_login);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_login:
+
+                break;
+        }
     }
 }
