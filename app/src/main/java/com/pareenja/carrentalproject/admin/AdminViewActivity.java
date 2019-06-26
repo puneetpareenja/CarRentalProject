149393ebf997555dccfe3fae9cc3d2c65163acd8
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-25 22:03:11 -0700

    Created new method displayNewFragment. For case "cars" of addFragment method displayed a new ManageCarsFragment

--- app/src/main/java/com/pareenja/carrentalproject/admin/AdminViewActivity.java
+++ app/src/main/java/com/pareenja/carrentalproject/admin/AdminViewActivity.java
@@ -1,6 +1,8 @@
 package com.pareenja.carrentalproject.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,25 +19,40 @@ public class AdminViewActivity
         setContentView(R.layout.activity_admin_view);
 
         AdminActionFragment adminActionFragment = new AdminActionFragment();
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.frame_admin,
-                        adminActionFragment,
-                        getResources().getString(R.string.admin_action_fragment))
-                .commit();
+        displayNewFragment(R.id.frame_admin,
+                adminActionFragment,
+                getResources().getString(R.string.admin_action_fragment),
+                false);
     }
 
     @Override
     public void addFragment(String string) {
         switch (string) {
             case "cars":
-                Toast.makeText(this, "Cars", Toast.LENGTH_SHORT).show();
+                ManageCarsFragment manageCarsFragment = new ManageCarsFragment();
+                displayNewFragment(R.id.frame_admin,
+                        manageCarsFragment,
+                        getResources().getString(R.string.manage_cars_fragment),
+                        true);
+
+
                 break;
             case "employees":
                 break;
             case "users":
                 break;
         }
+    }
+
+    public void displayNewFragment(int layoutId, Fragment fragment, String tag, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(layoutId, fragment, tag);
+
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(tag);
+        }
+
+        fragmentTransaction.commit();
     }
 }
