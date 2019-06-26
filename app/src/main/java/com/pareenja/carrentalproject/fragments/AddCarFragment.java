15295228f2caec9870cd1ca2427c686a197f5618
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-26 10:52:53 -0700

    Created a new Fragment - Add Car Fragment

---
+++ app/src/main/java/com/pareenja/carrentalproject/fragments/AddCarFragment.java
@@ -0,0 +1,32 @@
+package com.pareenja.carrentalproject.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pareenja.carrentalproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddCarFragment extends Fragment {
+
+
+    public AddCarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_car, container, false);
+    }
+
+}
