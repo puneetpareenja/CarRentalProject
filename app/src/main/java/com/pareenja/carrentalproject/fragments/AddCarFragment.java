da96b19500cab4808635b29fa3a24cd11384900b
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-06-27 01:29:47 -0700

    Initialized the layout in the AddCarFragment.java Implemented OnClickListener on AddCarFragment.java

--- app/src/main/java/com/pareenja/carrentalproject/fragments/AddCarFragment.java
+++ app/src/main/java/com/pareenja/carrentalproject/fragments/AddCarFragment.java
@@ -8,14 +8,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pareenja.carrentalproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddCarFragment extends Fragment {
+public class AddCarFragment extends Fragment implements View.OnClickListener {
 
+
+    private EditText vinEditText;
+    private EditText modelEditText;
+    private EditText brandEditText;
+    private EditText colorEditText;
+    private Button buttonAddCar;
 
     public AddCarFragment() {
         // Required empty public constructor
@@ -26,7 +34,24 @@ public class AddCarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_car, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_car, container, false);
+        initFragment(view);
+        return view;
     }
 
+    private void initFragment(View view) {
+        vinEditText = view.findViewById(R.id.edit_text_vin);
+        modelEditText = view.findViewById(R.id.edit_text_model);
+        brandEditText = view.findViewById(R.id.edit_text_brand);
+        colorEditText = view.findViewById(R.id.edit_text_color);
+        buttonAddCar = view.findViewById(R.id.button_add_car);
+
+        buttonAddCar.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
